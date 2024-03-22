de1c34aea9fd81b271e75bfba1ec35ad28480210
Samedy1 <samedyphin1@gmail.com>
2024-03-22 13:36:26 +0700

    small changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.session_state.logged_in = True
 # sleep(0.5)
 token = st.query_params['token']
 
-# ---------------------------------------------
+# -----------------------------------------------------------------------------------
 import data.preprocessed_data_v2 as pre
 
 pre.category_df = pre.get_df(api_config.categories_api_url, token)
@@ -20,14 +20,13 @@ pre.expense_df, income_df = pre.get_expense_income_df(pre.category_df)
 pre.preprocess_transaction_df = pre.transaction_df.copy()
 pre.preprocess_transaction_df = pre.preprocess_df(pre.preprocess_transaction_df)
 
-# ----------------------------------------------
-import data.data_handling as data_handling
-data_handling.df = data_handling.handle_missing_values(pre.preprocess_transaction_df)
-data_handling.df = data_handling.handle_duplicates(data_handling.df)
-# df = add_date_columns(preprocess_df, "transaction_date")
+# ---------------------------------------------------------------------------------
+import data.data_handling as handler
+handler.df = handler.handle_missing_values(pre.preprocess_transaction_df)
+handler.df = handler.handle_duplicates(handler.df)
 
 # Sort column
 sort_columns = ['year', 'month', 'day']
-df = data_handling.sort_dataframe(data_handling.df, sort_columns)
+df = handler.sort_dataframe(handler.df, sort_columns)
 
 st.switch_page("pages/page1_overview.py")
