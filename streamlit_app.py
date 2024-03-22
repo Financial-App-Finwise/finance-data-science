f4e51a6ff6f1d1cbf424771f974fa75e5bfbec0b
Samedy1 <samedyphin1@gmail.com>
2024-03-22 13:33:26 +0700

    small changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,13 @@ token = st.query_params['token']
 
 # ---------------------------------------------
 import data.preprocessed_data_v2 as pre
-from data.preprocessed_data_v2 import *
 
-pre.category_df = get_df(api_config.categories_api_url, token)
-pre.transaction_df = get_df(api_config.transaction_api_url, token)
-pre.expense_df, income_df = get_expense_income_df(pre.category_df)
+pre.category_df = pre.get_df(api_config.categories_api_url, token)
+pre.transaction_df = pre.get_df(api_config.transaction_api_url, token)
+pre.expense_df, income_df = pre.get_expense_income_df(pre.category_df)
 
 pre.preprocess_transaction_df = pre.transaction_df.copy()
-pre.preprocess_transaction_df = preprocess_df(pre.preprocess_transaction_df)
+pre.preprocess_transaction_df = pre.preprocess_df(pre.preprocess_transaction_df)
 
 # ----------------------------------------------
 import data.data_handling as data_handling
